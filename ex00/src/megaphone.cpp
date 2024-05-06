c45475a73f1052fb1e402572ff94a17496262c54
Simon Dablander <ifaoji@pm.me>
2024-05-06 18:01:09 +0200

    feat: support megaphone args

--- ex00/src/megaphone.cpp
+++ ex00/src/megaphone.cpp
@@ -1,8 +1,21 @@
+#include <cctype>
 #include <iostream>
 
 int main(int argc, char **argv) {
   if (argc == 1) {
     std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+
+    return 0;
   }
-  (void)argv;
+
+  for (int i = 1; i < argc; i++) {
+    std::string output(argv[i]);
+
+    for (size_t j = 0; j < output.length(); j++)
+      output[j] = toupper(output[j]);
+
+    std::cout << output;
+  }
+
+  std::cout << std::endl;
 }
